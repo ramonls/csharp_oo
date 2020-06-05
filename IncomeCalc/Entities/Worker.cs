035feb562c778311ef91f85df630e172e4d4c54a
Ramon Lima <ramon.lima@outlook.com>
2020-06-05 14:11:25 -0300

    Trabalhando na classe Worker

--- IncomeCalc/Entities/Worker.cs
+++ IncomeCalc/Entities/Worker.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using IncomeCalc.Entities.Enums;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IncomeCalc.Entities
 {
     class Worker
     {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Departament { get; set; } //Associação entre duas classes
+        // Como vamos associar a classe HourContract e ela poder ter 1 ou máis, vamos criar em uma lista,
+        // vamos instanciar para ela não ser nula
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
     }
 }
