ab4a6daedc3547d34bfbacaf70477bb7cc00049a
Ramon Lima <ramon.lima@outlook.com>
2020-06-05 12:46:04 -0300

    Criando o projeto IncomeCalc / Criando as classes Departament e HourContratc

---
+++ IncomeCalc/Entities/Departament.cs
@@ -0,0 +1,20 @@
+﻿
+namespace IncomeCalc.Entities
+{
+    class Departament
+    {
+        public string Name { get; set; }
+
+        //COnstrutor padrão
+        public Departament()
+        {
+
+        }
+
+        //Construtor que recebe o nome
+        public Departament(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ IncomeCalc/Entities/HourContract.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeCalc.Entities
+{
+    class HourContract
+    {
+        //propriedades
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        //Construtor padrão
+        public HourContract()
+        {
+
+        }
+
+        //Construtor com os três argumentos
+        public HourContract(DateTime date, double valuePerHours, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHours;
+            Hours = hours;
+        }
+
+        //método
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
