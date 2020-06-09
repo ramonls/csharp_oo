60cb8554888f6d78c3ffca00543c809c7f493cbf
Ramon Lima <ramon.lima@outlook.com>
2020-06-09 11:37:18 -0300

    Finalizando as classes

--- IncomeCalc/Entities/Worker.cs
+++ IncomeCalc/Entities/Worker.cs
@@ -14,5 +14,45 @@ namespace IncomeCalc.Entities
         // vamos instanciar para ela não ser nula
         public List<HourContract> Contracts { get; set; } = new List<HourContract>();
 
+        //construtor padrão
+        public Worker()
+        {
+
+        }
+        /*Construtor passando parâmetros
+          Sempre quando tivermos uma associação para muitos não é usuál passar uma lista instanciada em um construtor do objeto.
+          Essa lista comerá vazia e depois irá adicionando elementos conforme a necessidade.
+          SEMPRE QUE TIVER UMA ASSOCIAÇÃO PARA MUITOS, ELA NÃO SERÁ INCLUSA NO CONSTRUTOR!!!        
+         */
+        public Worker(string name, WorkerLevel level, double baseSalary, Departament departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary; // Ele recebe o BaseSalary, pq é o minimo que ele irá ganhar.
+
+            foreach(HourContract contract in Contracts)
+            {
+                if(contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
+
     }
 }
