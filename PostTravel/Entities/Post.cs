165d58306045d07ff36324a6d85bf7496f865fc6
Ramon Lima <ramon.lima@outlook.com>
2020-06-09 17:37:04 -0300

    Finalizando programa

--- PostTravel/Entities/Post.cs
+++ PostTravel/Entities/Post.cs
@@ -37,5 +37,38 @@ namespace PostTravel.Entities
         {
             Comments.Remove(comment);
         }
+
+        /*
+         * Como temos muitas informações a ser mostradas, e se for acrescentado mais comentarios teremos
+         * que acrescentar mais informações, e ficar concatenando string, irá degradar a performece da aplicação,
+         * para facilitar, vamos usar o StringBuilder.
+         * 
+         * StringBuilder --> Ele é responsável por fazer uma montagem de uma string, conforme o programador mandar
+         * de uma forma muito eficiente, e depois quando tiver pronto, pegamos o resultado em forma de string.
+         * Para usar ele, temos que instancia-lo, ele pertence ao namespace System.text;
+         * Append (acrescentar)
+         * 
+         * 
+         * 
+         */
+
+        public override string ToString()
+        {
+            //instanciar um objeto StringBuilder
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+
+            foreach(Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+
+            return sb.ToString(); //Converter todo o conteúdo do sb que é um StringBuilder em string
+        }
     }
 }
--- PostTravel/Program.cs
+++ PostTravel/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using PostTravel.Entities;
 
 namespace PostTravel
 {
@@ -10,6 +9,20 @@ namespace PostTravel
     {
         static void Main(string[] args)
         {
+            //Tudo vai ser instanciado manualmente
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome!");
+            Post p1 = new Post(DateTime.Parse("21/06/2018 13:05:44"), "Traveling to New Zealand", "I'm goig to visit this wonderful country!", 12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with you");
+            Post p2 = new Post(DateTime.Parse("28/07/2018 23:14:19"), "Good night guys","See you tomorrow",5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+
         }
     }
 }
