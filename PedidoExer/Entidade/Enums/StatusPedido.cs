fba1808416bf00a914ef04ba2e263d141056024b
Ramon Lima <ramon.lima@outlook.com>
2020-06-10 12:13:17 -0300

    Adicionado o enum StatusPedido

---
+++ PedidoExer/Entidade/Enums/StatusPedido.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PedidoExer.Entidade.Enums
+{
+    enum StatusPedido : int
+    {
+        PagamentoPendente = 0,
+        Processando = 1,
+        Enviado = 2,
+        Entregue = 3
+    }
+}
