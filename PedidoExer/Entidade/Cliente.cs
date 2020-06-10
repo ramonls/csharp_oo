fe476cbee70b67a69fda6934a0e7adc7687e725b
Ramon Lima <ramon.lima@outlook.com>
2020-06-10 12:42:00 -0300

    Criação da classe Cliente

---
+++ PedidoExer/Entidade/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidoExer.Entidade
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        DateTime DataNascimento { get; set; }
+
+        //construtor
+        public Cliente()
+        {
+        }
+
+        public Cliente(string nome, string email, DateTime dataNascimento)
+        {
+            Nome = nome;
+            Email = email;
+            DataNascimento = dataNascimento;
+        }
+    }
+}
