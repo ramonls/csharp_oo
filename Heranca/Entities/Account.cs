abcfcbb383e71f0d6bbfb6d05853605b46ed99e9
Ramon Lima <ramon.lima@outlook.com>
2020-06-11 11:24:08 -0300

    Criação da classe Account

---
+++ Heranca/Entities/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+
+        //construtor
+        public Account()
+        {
+        }
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        //Método para retirada de dinheiro
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+        //Método para deposito
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+
+
+
+
+
+    }
+}
