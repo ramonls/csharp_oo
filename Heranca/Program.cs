37cad994a6635b9bd94ccf333262bc02b192df5d
Ramon Lima <ramon.lima@outlook.com>
2020-06-18 12:47:03 -0300

    Finalizando o programa sobre Upcasting e Downcasting

--- Heranca/Program.cs
+++ Heranca/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Heranca.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,33 @@ namespace Heranca
     {
         static void Main(string[] args)
         {
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //Upcasting
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+            //DownCasting
+            BusinessAccount acc4 = (BusinessAccount)acc3;
+            acc4.Loan(100.0);
+
+            //Outra forma de converter (casting) usando a palavra reservada as
+            BusinessAccount acc6 = acc3 as BusinessAccount;
+
+            /*
+            * BusinessAccount acc5 = (BusinessAccount) acc3;
+            * a variável acc3 é do tipo SavingsAccount, visto as duas ser subclasse, o sistema irá apresentar
+            * o erro na execução do sistema, para que possamos testar se uma variável é de um tipo usamos
+            * a palavra reservada is
+            */
+            //Se a variável acc3 for do tipo BusinessAccount --> Irá atribuir e converter <--
+            if(acc3 is BusinessAccount)
+            {
+                BusinessAccount acc5 = (BusinessAccount)acc3;
+            }
+
         }
     }
 }
