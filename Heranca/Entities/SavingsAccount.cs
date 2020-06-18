16c7937a58c2bb6879fb8c76ee069dc29edbf5bf
Ramon Lima <ramon.lima@outlook.com>
2020-06-18 11:56:04 -0300

    Criação da classe SavingAccount (conta poupança)

---
+++ Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InteresRate { get; set; }
+
+        //construtores
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount (int number, string holder, double balance, double interesRate) : base(number, holder, balance)
+        {
+            InteresRate = interesRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InteresRate;
+        }
+    }
+}
