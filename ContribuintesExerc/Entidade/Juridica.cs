49830521bbf32a58483d4662c8f99838781e8fe1
Ramon Lima <ramon.lima@outlook.com>
2020-06-22 18:44:19 -0300

    Criação da classe Juridica

---
+++ ContribuintesExerc/Entidade/Juridica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContribuintesExerc.Entidade
+{
+    class Juridica : Pessoa
+    {
+        public int NumeroFuncionario { get; set; }
+
+        public Juridica(int numeroFuncionario, string nome, double rendaAnual):base(nome, rendaAnual)
+        {
+            NumeroFuncionario = numeroFuncionario;
+        }
+
+        public override double Taxa()
+        {
+            double imposto;
+            if(NumeroFuncionario > 10)
+            {
+                imposto = (RendaAnual * 0.14);
+            }
+            else
+            {
+                imposto = (RendaAnual * 0.16);
+            }
+
+            return imposto;
+        }
+    }
+}
