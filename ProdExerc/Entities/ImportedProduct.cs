a8785d19df1f1922875bd6c88409802983bcd8a5
Ramon Lima <ramon.lima@outlook.com>
2020-06-22 12:06:57 -0300

    Criação da classe ImportedProduct

---
+++ ProdExerc/Entities/ImportedProduct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+namespace ProdExerc.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+        public ImportedProduct(string name, double price, double customsFess): base(name, price)
+        {
+            CustomsFee = customsFess;
+        }
+
+        public double TotalPrice()
+        {
+            return Price += CustomsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return base.PriceTag() + $"(Custom fee: $ {CustomsFee.ToString("F2", CultureInfo.InvariantCulture)})";
+        }
+    }
+}
