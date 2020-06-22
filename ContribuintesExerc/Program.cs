f833fb987a258f37bbf23be89227f7bb1a830f82
Ramon Lima <ramon.lima@outlook.com>
2020-06-22 19:09:02 -0300

    Finalização do projeto ContribuintesExerc

--- ContribuintesExerc/Program.cs
+++ ContribuintesExerc/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Runtime.Serialization.Formatters;
+using System.Globalization;
+using System.Collections.Generic;
+using ContribuintesExerc.Entidade;
 
 namespace ContribuintesExerc
 {
@@ -6,7 +10,44 @@ namespace ContribuintesExerc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Pessoa> lista = new List<Pessoa>();
+            Console.Write("Entre com o numero de pagamentos de taxa: ");
+            int numeroTaxa = int.Parse(Console.ReadLine());
+
+            for(int i=1; i<= numeroTaxa; i++)
+            {
+                Console.WriteLine($"Informações do pagamento da taxa #{i}:");
+                Console.Write("Física ou Juridica (f/j): ");
+                char tipo = char.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Renda Anual: R$");
+                double rendaAnual = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if(tipo == 'f')
+                {
+                    Console.Write("Despesa Médica: R$");
+                    double despesaMedica = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    lista.Add(new Fisica(despesaMedica, nome, rendaAnual));
+                }
+                if(tipo == 'j')
+                {
+                    Console.Write("Numero de Empregados: ");
+                    int numeroEmpregado = int.Parse(Console.ReadLine());
+                    lista.Add(new Juridica(numeroEmpregado, nome, rendaAnual));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pagamentos de Taxa:");
+            double somaTaxa = 0;
+            foreach (Pessoa pessoa in lista)
+            {
+                Console.WriteLine(pessoa.Nome + " : R$" + pessoa.Taxa().ToString("F2", CultureInfo.InvariantCulture));
+                somaTaxa += pessoa.Taxa();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Total Taxas: R$" + somaTaxa.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
         }
     }
 }
