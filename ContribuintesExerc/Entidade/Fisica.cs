76232b2d4fcd7e22264f4e7d19190b3b1d988471
Ramon Lima <ramon.lima@outlook.com>
2020-06-22 18:31:05 -0300

    Criação da classe Fisica que estende a Pessoa

---
+++ ContribuintesExerc/Entidade/Fisica.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContribuintesExerc.Entidade
+{
+    class Fisica : Pessoa
+    {
+        public double GastoComSaude { get; set; }
+
+        public Fisica(double gastoComSaude, string nome, double rendaAnual): base(nome, rendaAnual)
+        {
+            GastoComSaude = gastoComSaude;
+        }
+
+        public override double Taxa()
+        {
+            double imposto;
+            if (RendaAnual < 20000.00)
+            {
+                imposto = (RendaAnual * 0.15) - (GastoComSaude * 0.5);
+            }
+            else
+            {
+                imposto = (RendaAnual * 0.25) - (GastoComSaude * 0.5);
+            }
+            return imposto;
+        }
+    }
+}
