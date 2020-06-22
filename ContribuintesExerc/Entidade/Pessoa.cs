5b339e7300ba7ffa98df4374bd480a512a85aaa2
Ramon Lima <ramon.lima@outlook.com>
2020-06-22 18:19:05 -0300

    Criação da classe abstrata Pessoa

---
+++ ContribuintesExerc/Entidade/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContribuintesExerc.Entidade
+{
+    abstract class Pessoa
+    {
+        public String Nome { get; set; }
+        public double RendaAnual { get; set; }
+
+        public Pessoa (string nome, double rendaAnual)
+        {
+            Nome = nome;
+            RendaAnual = rendaAnual;
+        }
+
+        public abstract double Taxa();
+    }
+}
