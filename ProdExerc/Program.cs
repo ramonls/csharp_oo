effdaf68dcf1b204683f06379c61e602261b1507
Ramon Lima <ramon.lima@outlook.com>
2020-06-22 11:16:38 -0300

    Criação do projeto ProdExerc

---
+++ ProdExerc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProdExerc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
