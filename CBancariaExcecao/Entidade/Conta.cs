9e10a5e092cc480dfa173f9613f2322fcfdd8ae7
Ramon Lima <ramon.lima@outlook.com>
2020-06-23 18:11:58 -0300

    Criação da classe Conta e começando estrutura do Program

---
+++ CBancariaExcecao/Entidade/Conta.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using CBancariaExcecao.Entidade.Excecao;
+
+namespace CBancariaExcecao.Entidade
+{
+    class Conta
+    {
+        public int Numero { get; set; }
+        public string Nome { get; set; }
+        public double Saldo { get; set; }
+        public double LimiteRetirada { get; set; }
+
+        public Conta(int numero, string nome, double saldo, double limiteRetirada)
+        {
+            if (limiteRetirada < 0)
+                throw new CBExcecao("Limite de retirada não pode ser menor que zero!");
+            try
+            {
+                Numero = numero;
+                Nome = nome;
+                Saldo = saldo;
+                LimiteRetirada = limiteRetirada;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro no construtor: "+e);
+            }    
+        }
+
+        public void Deposito(double montante)
+        {
+            try
+            {
+                Saldo += montante;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro no deposito: "+e);
+            }
+        }
+        public void Retirada(double montante)
+        {
+            if (montante > LimiteRetirada)
+                throw new CBExcecao("Erro - Valor superior ao limite de retirada!");
+            if (montante > Saldo)
+                throw new CBExcecao("Erro - Valor superior ao saldo!");
+            
+            Saldo -= montante;
+        }
+
+    }
+}
--- CBancariaExcecao/Program.cs
+++ CBancariaExcecao/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using CBancariaExcecao.Entidade;
+using CBancariaExcecao.Entidade.Excecao;
 
 namespace CBancariaExcecao
 {
@@ -6,7 +9,22 @@ namespace CBancariaExcecao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Enntre com as informações da conta:");
+                Console.Write("Numero: ");
+                int numero = int.Parse(Console.ReadLine());
+                Console.Write("Titular: ");
+                string titular = Console.ReadLine();
+                Console.Write("Saldo Inicial: R$");
+                double saldoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Limite de retirada: R$");
+                double limiteRetirada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            }
+            
+
         }
     }
 }
