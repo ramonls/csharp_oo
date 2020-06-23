73c5faeb66641b49797f85acb79769968e4184e2
Ramon Lima <ramon.lima@outlook.com>
2020-06-23 17:38:05 -0300

    Criação da classe para controle de exceção CBExcecao

---
+++ CBancariaExcecao/Entidade/Excecao/CBExcecao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CBancariaExcecao.Entidade.Excecao
+{
+    class CBExcecao : ApplicationException
+    {
+        //construtor
+        public CBExcecao(string message): base(message)
+        {
+        }
+    }
+}
