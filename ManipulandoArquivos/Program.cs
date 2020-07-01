50117e981372a4c1ac2f74cd1212141b4eb8d2e5
Ramon Lima <ramon.lima@outlook.com>
2020-07-01 12:52:29 -0300

    Usando StreamReader e o FileStream e fechando-os manualmente

--- ManipulandoArquivos/Program.cs
+++ ManipulandoArquivos/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Data;
 
 namespace ManipulandoArquivos
 {
@@ -11,7 +12,65 @@ namespace ManipulandoArquivos
     {
         static void Main(string[] args)
         {
+            //Sem o @ teriamos que colocar duas barras. EX: "C:\\csharp_aula\\arquivo1.txt"
+            string caminhoOrigem = @"C:\csharp_aula\arquivo1.txt";
+            string caminhoDestino = @"C:\csharp_aula\arquivo2.txt";
 
+            try
+            {
+                FileInfo fileInfo = new FileInfo(caminhoOrigem);
+                //faz uma cópia do arquivo
+                //fileInfo.CopyTo(caminhoDestino);
+                //Criamos um vetor linhas, ele vai receber todas as linhas que estão no arquivo caminhOrigem
+                string[] linhas = File.ReadAllLines(caminhoOrigem);
+                foreach(string linha in linhas)
+                {
+                    Console.WriteLine(linha);
+                }
+
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("<o> Erro-> "+e.Message);
+            }
+
+            //Usando FileStream e StreamReader
+            Console.WriteLine();
+            Console.WriteLine("#########################################");
+
+            FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                //fs = new FileStream(caminhoOrigem, FileMode.Open);
+                //sr = new StreamReader(fs);
+                sr = File.OpenText(caminhoOrigem);
+                while (!sr.EndOfStream)
+                {
+                    string linha = sr.ReadLine();
+                    Console.WriteLine("Linha do arquivo -> " + linha);
+                }
+               
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("<o> Erro -> "+e.Message);
+            }
+            finally
+            {
+                //Fechando manualmente sr e fs
+                //se o sr e o fs estiver aberto
+                if (sr != null) sr.Close();
+                //if (fs != null) fs.Close();
+                
+            }
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
