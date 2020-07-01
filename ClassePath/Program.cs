da4351a42a216621a46cb1570a979bd2c5827fee
Ramon Lima <ramon.lima@outlook.com>
2020-07-01 16:40:43 -0300

    Projeto ClassePath (criado/finalizado)

---
+++ ClassePath/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassePath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminho = @"C:\csharp_aula\arquivo1.txt";
+
+            Console.WriteLine("GetDirectoryName: "+Path.GetDirectoryName(caminho));
+            Console.WriteLine("GetFileName: "+Path.GetFileName(caminho));
+            Console.WriteLine("GetExtension: "+Path.GetExtension(caminho));
+            Console.WriteLine("GetFileNameWhithoutExtension: "+Path.GetFileNameWithoutExtension(caminho));
+            Console.WriteLine("GetFullPath: "+Path.GetFullPath(caminho));
+
+            Console.ReadLine();
+        }
+    }
+}
