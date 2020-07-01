81b53c97207ae86d2a79a618080d09bbf58f8d8b
Ramon Lima <ramon.lima@outlook.com>
2020-07-01 16:28:53 -0300

    Fianlização do projeto ManipulandoDiretorio

--- ManipulandoDiretorio/Program.cs
+++ ManipulandoDiretorio/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ManipulandoDiretorio
 {
@@ -10,6 +11,34 @@ namespace ManipulandoDiretorio
     {
         static void Main(string[] args)
         {
+            string caminho = @"C:\csharp_aula";
+
+            try
+            {
+                //Pastas
+                IEnumerable<string> pastas = Directory.EnumerateDirectories(caminho, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Pastas:");
+                foreach(string pasta in pastas)
+                {
+                    Console.WriteLine(pasta);
+                }
+                //Arquivos
+                IEnumerable<string> arquivos = Directory.EnumerateFiles(caminho, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Arquivos:");
+                foreach (string arquivo in arquivos)
+                {
+                    Console.WriteLine(arquivo);
+                }
+
+                Directory.CreateDirectory(caminho + @"\Nova Pasta");
+            
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("Erro: " + e.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
