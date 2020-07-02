30da1ba6d815308d60df92da17efbf31068d979d
Ramon Lima <ramon.lima@outlook.com>
2020-07-02 11:14:40 -0300

    Criação/Finalização do projeto ArqCSVExerc

---
+++ ArqCSVExerc/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+
+namespace ArqCSVExerc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string ArquivoCSV = @"C:\csharp_aula\vendas.csv";
+
+            try
+            {
+                using (FileStream arquivo = new FileStream(ArquivoCSV, FileMode.Open))
+                {
+                    using (StreamReader leituraArquivo = new StreamReader(arquivo))
+                    {
+                        while (!leituraArquivo.EndOfStream)
+                        {
+                            string linha = leituraArquivo.ReadLine();
+                            string[] elemento = linha.Split(',');
+                            double preco = double.Parse(elemento[1], CultureInfo.InvariantCulture);
+                            int quantidade = int.Parse(elemento[2]);
+                            Console.WriteLine(elemento[0]+", "+quantidade*preco);
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("Erro: "+e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
