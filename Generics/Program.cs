2682a4907925123d07dca2e6850273dbb662deaa
Ramon Lima <ramon.lima@outlook.com>
2020-07-08 12:41:13 -0300

    Criação do projeto Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrintService<string> printService = new PrintService<string>(); PrintService do tipo string
+            PrintService<int> printService = new PrintService<int>(); //PrintService do tipo int
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: "+printService.First());
+            Console.ReadLine();
+        }
+    }
+}
