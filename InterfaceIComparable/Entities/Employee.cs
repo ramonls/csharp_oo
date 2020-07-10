c133da6a99dd6a35eee28eb2e8f8b2631151c827
Ramon Lima <ramon.lima@outlook.com>
2020-07-10 12:28:56 -0300

    Criação e finalização do projeto InterfaceIComparable

---
+++ InterfaceIComparable/Entities/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography;
+
+namespace InterfaceIComparable.Entities
+{
+    class Employee: IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        //construtor
+        public Employee(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            //verificar se o obj é do tipo Employee
+            if(!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: argument is not an Employee");
+            }
+            //Fazendo um downcasting
+            Employee other = obj as Employee;
+            //Aqui estamos usando o ComparteTo da classe string, pois o nome do funcionário é do tipo string
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ InterfaceIComparable/Program.cs
@@ -0,0 +1,44 @@
+﻿using InterfaceIComparable.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceIComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Arquivos\Ramon\Cursos\Udemy\Andamento\CSharp\projetos\CSharpOO\Employee.csv";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    //ordenar a lista
+                    list.Sort();
+
+                    //mostrar a lista, como já tem um ToString() formatado na classe, vamos apenas chamar ela
+                    foreach(Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
