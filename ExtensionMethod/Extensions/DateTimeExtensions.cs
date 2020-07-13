ed16af7cedfde674ce72e0695e942215720e0df1
Ramon Lima <ramon.lima@outlook.com>
2020-07-13 18:12:05 -0300

    Criação / Finalização do projeto ExtensionMethod

---
+++ ExtensionMethod/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExtensionMethod.Extensions
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if(duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " horas";
+            }
+            else //if(duration.TotalDays < 365)
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " Dias";
+            }
+            /*else
+            {
+                string anos = (duration.TotalDays / 365).ToString("F1", CultureInfo.InvariantCulture);
+                return anos + " Anos";
+            }*/
+        }
+    }
+}
---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using ExtensionMethod.Extensions;
+using System;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criar uma extensão de método
+            DateTime dt = new DateTime(2020, 6, 12, 16, 30, 15);
+            Console.WriteLine(dt.ElapsedTime());
+            
+            Console.WriteLine();
+            string s1 = "De gole em gole, eu ajudo a salvar o planeta!";
+            Console.WriteLine(s1.Cut(10));
+
+
+            Console.ReadLine();
+        }
+    }
+}
