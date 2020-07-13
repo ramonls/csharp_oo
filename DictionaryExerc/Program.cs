480ef4309d6580ac4af1ba05ed66ad117aa20fa2
Ramon Lima <ramon.lima@outlook.com>
2020-07-13 16:44:49 -0300

    Finalização do projeto DictionaryExerc

--- DictionaryExerc/Program.cs
+++ DictionaryExerc/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace DictionaryExerc
 {
@@ -6,7 +8,34 @@ namespace DictionaryExerc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            Dictionary<string, int> eleicao = new Dictionary<string, int>();
+
+            using (StreamReader sr = new StreamReader(File.OpenRead(path)))
+            {
+                while (!sr.EndOfStream)
+                {
+
+                    string[] line = sr.ReadLine().Split(',');
+                    string candidato = line[0];
+                    int votos = int.Parse(line[1]);
+
+                    if (eleicao.ContainsKey(candidato))
+                    {
+                        eleicao[candidato] += votos;
+                    }
+                    else
+                    {
+                        eleicao[candidato] = votos;
+                    }
+                }
+                foreach(var item in eleicao)
+                {
+                    Console.WriteLine(item.Key+": "+item.Value);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
