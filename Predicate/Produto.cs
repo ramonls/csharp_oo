db526b3e744e87cf2d9226040144b0ea830ee241
Ramon Lima <ramon.lima@outlook.com>
2020-07-16 15:07:04 -0300

    Criação/Finalização do projeto Predicate

---
+++ Predicate/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Predicate
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public override string ToString()
+        {
+            return Nome + " - R$" + Preco.ToString("F2");
+        }
+    }
+}
---
+++ Predicate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> listaProdutos = new List<Produto>();
+
+            listaProdutos.Add(new Produto("TV 4K", 2565.36));
+            listaProdutos.Add(new Produto("Radio", 652.65));
+            listaProdutos.Add(new Produto("Mouse", 65.99));
+            listaProdutos.Add(new Produto("Tablet", 899.99));
+
+            //neste caso estamos usando uma expressão lambda
+            //remover todos o objeto p tal que p.preco seja menor que 700.00
+            //listaProdutos.RemoveAll(p => p.Preco < 700.0);
+
+            //Porém, poderíamos criar uma função substituiria a expressão lambda
+            listaProdutos.RemoveAll(ComparaPreco);
+
+
+            foreach(Produto item in listaProdutos)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+        //função que substituiria a expressão lambda, em ocasiões que dê para usar a expressão lambda é melhor.
+        public static bool ComparaPreco(Produto obj)
+        {
+            return obj.Preco < 700.00;
+        }
+    }
+}
