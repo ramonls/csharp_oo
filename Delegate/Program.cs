6abfaa06b3969ea11b9c79300671a8020bcb950c
Ramon Lima <ramon.lima@outlook.com>
2020-07-16 13:25:41 -0300

    Criado / Finalizado o projeto Delegate

---
+++ Delegate/Program.cs
@@ -0,0 +1,51 @@
+﻿using Delegate.Services;
+using System;
+
+namespace Delegate
+{
+    /* Exemplo 1 - este exemplo retorna um valor do tipo double
+     * Aqui declaramos um delegate, esse delegate está informando que ele vai ser uma referência a uma
+     * função, que receberá dois números double. e retorna um número double.
+     * 
+    delegate double OperacaoNumeroBinario(double n1, double n2);
+    */
+
+    //Exemplo 2 - referenciando mais funções ao delegate, e sem retorno (void) [MultiCast]
+    delegate void OperacaoNumeroBinario(double x, double y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            /* Exemplo 1
+            //Vamos declarar um objeto do tipo OperacaoNumeroBinario, recebendo um dos  método da nossa classe CalculationServices
+            //O objeto operacao é uma referência para a função Sum que está em CalculationServices.
+            OperacaoNumeroBinario operacao = CalculationServices.Sum;
+            //>>> Outra sintaxe alternativa
+            //OperacaoNumeroBinario operacao = new OperacaoNumeroBinario(CalculationServices.Sum);
+            //Porém fica mais verboso.
+
+
+            //Criamos uma variavel que receberá o nosso objeto delegate que está referenciando a função sum.
+            double resultado = operacao(a, b);
+            //>>> Sintaxe alternativa
+            //Se desejar poderemos utilizar a palavra reservada Invoke, para invocar a função, porém é o mesmo resultado acima.
+            //double resultado = operacao.Invoke(a, b);
+            Console.WriteLine(resultado);
+            */
+
+            //Objeto do tipo delegate recebendo a referencia do método da classe
+            OperacaoNumeroBinario operacao = CalculationServices.ShowSum;
+            //Aqui vamos fazer um multicast para que ele receba a referência do outro método também, executando as duas funções
+            operacao += CalculationServices.ShowMax;
+
+            //Como ele é void, não tem retorno vamos chamar ele da seguinte maneira:
+            operacao.Invoke(a, b);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Delegate/Services/CalculationServices.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate.Services
+{
+    class CalculationServices
+    {
+        /* Exemplo 1
+        //Uma classe com métodos para calcular (Número máximo, soma e quadrado)
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x : y;
+        }
+
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Square(double x)
+        {
+            return x * x;
+        }*/
+
+        //Exemplo 2 - agora eles não retornam e já apresenta o valor na tela.
+
+        public static void ShowMax(double x, double y)
+        {
+            double max = (x > y) ? x : y;
+            Console.WriteLine(max);
+        }
+        public static void ShowSum(double x, double y)
+        {
+            double sum = x + y;
+            Console.WriteLine(sum);
+        }
+
+    }
+}
