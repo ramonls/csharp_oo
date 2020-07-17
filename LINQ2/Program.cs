a654f93d289b7d215e98208ee129bbaa30df1c6c
Ramon Lima <ramon.lima@outlook.com>
2020-07-17 18:37:29 -0300

    Finalização do projeto LINQ2

--- LINQ2/Program.cs
+++ LINQ2/Program.cs
@@ -36,7 +36,7 @@ namespace LINQ2
                 new Product() {Id = 3, Name = "Hammer", Price = 90.0, Category = c1},
                 new Product() {Id = 4, Name = "TV", Price = 1700.0, Category = c3},
                 new Product() {Id = 5, Name = "Notebook", Price = 1300.0, Category = c2},
-                new Product() {Id = 6, Name = "Saw", Price = 00.0, Category = c1},
+                new Product() {Id = 6, Name = "Saw", Price = 80.0, Category = c1},
                 new Product() {Id = 7, Name = "Tablet", Price = 700.0, Category = c2},
                 new Product() {Id = 8, Name = "Camera", Price = 700.0, Category = c3},
                 new Product() {Id = 9, Name = "Printer", Price = 350.0, Category = c3},
@@ -87,6 +87,52 @@ namespace LINQ2
             */
             var resultado6 = products.Where(p => p.Id == 3).SingleOrDefault();
             Console.WriteLine("Usando o SingleOrDefaul para um retorno de um unico elemento: "+resultado6);
+
+            //vamos utilizar o Max para buscar o maio preço dos produtos
+            //se não usarmos a expressão lambda ele espera que o seu objeto tenho um método de comparação IComparable
+            //se não ele retorna uma exceção
+            //Podemos usar o Min() para um resultado oposto, mínimo.
+            var resultado7 = products.Max(p => p.Price);
+            Console.WriteLine("O maior preço: "+ resultado7);
+
+            //vamos obter a soma dos produtos da categoria 1
+            var resultado8 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
+            Console.WriteLine("Soma dos produtos da categoria 1: "+ resultado8);
+
+            //Podemos fazer a média com o Average(), seguindo os mesmos passos do Sum
+            //se der uma colação vazia ele vai apresentar uma exceção.
+            //vamos fazer um exemplo de uma categoria que não existe, porém vamos utilizar o DefaultIfEmpty()
+            //que no caso iremos verificar se ele for nulo, podemos colocar como argumento um valor a ser apresentado
+            //Estamos pesquisando os produtos da categoria 5, convertendo para double com o select, defindo um valor caso
+            //a coleção retorne vazia (0.0) e depois estamos pedindo para fazer a média, caso a coleção não esteja vazia
+            var resultado9 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Médias dos valores da Categoria 5: "+resultado9);
+
+            //Em outras linguagens tem o nome de Map / Redux
+            //Aqui em C# tem o nome de Select / Aggregate
+            //Vamos fazer uma soma manual dos produtos da categoria 1
+            //no aggregate vamos fazer uma função anonima, e antes vamos colocar um valor padrão
+            //para se caso ele não achar nenhuma elemento na coleção para fazer a soma, ele irá utilizar o
+            //valor padrão que na soma será 0 e 0.0 para double
+            var resultado10 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
+            Console.WriteLine("Soma (manual) dos valor da categoria 1 "+ resultado10);
+            Console.WriteLine();
+            //Operação de agrupamento
+            //cada elemento retorna do tipo IGrouping<Category, Product>
+            var resultado11 = products.GroupBy(p => p.Category);
+            //O tipo para o foreach terá que ser do mesmo tipo para ele percorre e gravar na variavel
+            //terá uma chave Category e uma coleção Product
+            foreach(IGrouping<Category, Product> group in resultado11)
+            {
+                Console.WriteLine("Categoria "+group.Key.Name+":");
+                //vamos fazer outro foreach para imprimir os produtos dessa categoria
+                foreach(Product p in group)
+                {
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine();
+
+            }
 
             Console.ReadLine();
         }
